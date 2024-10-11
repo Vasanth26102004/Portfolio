2f47d43cc93df81cde8c3a4d897ce9ad6a46e72d
Vasanth <157830407+Vasanth26102004@users.noreply.github.com>
2024-10-11 22:36:02 +0530

    Update project.js

--- src/project.js
+++ src/project.js
@@ -10,13 +10,13 @@ const project = [
     technologies: "HTML, CSS, JAVASCRIPT",
   },
   {
-    id: "1",
-    name: "Whack-A-Mole",
+    id: "2",
+    name: "Calculater",
     description:
-      "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
+      "A calculator app is a software tool designed to perform basic arithmetic operations (addition, subtraction, multiplication, division)",
     image: "https://www.marketjs.com/item/whack-a-mole/whack-a-mole.jpg",
-    repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
-    preview: "https://vasanth26102004.github.io/Whack-A-Mole/",
+    repo: "https://github.com/Vasanth26102004/Calculater.git",
+    preview: "https://vasanth26102004.github.io/Calculater/",
     technologies: "HTML, CSS, JAVASCRIPT",
   },
   {
