838175c80ece1f4d1cbb10a4dab8c8e00271dd9b
Vasanth26102004 <vasanth.p2610@gmil.com>
2024-10-11 13:39:33 +0530

    next commit

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,12 +5,6 @@ import resume from "../assets/vasanth.pdf";
 import resumeimg from "../assets/vasanth.jpg";
 
 const Resume = () => {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
   return (
     <div className="resume">
       <a href={resume}>
