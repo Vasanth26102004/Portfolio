3407d2bf31ec77a606f87a7122729f46980f5aa0
Vasanth <157830407+Vasanth26102004@users.noreply.github.com>
2024-10-25 21:06:33 +0530

    Update project.js

--- src/project.js
+++ src/project.js
@@ -31,13 +31,13 @@ const project = [
   },
   {
     id: "1",
-    name: "Whack-A-Mole",
+    name: "Daily Do",
     description:
       "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
     image: "https://www.marketjs.com/item/whack-a-mole/whack-a-mole.jpg",
-    repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
-    preview: "https://vasanth26102004.github.io/Whack-A-Mole/",
-    technologies: "HTML, CSS, JAVASCRIPT",
+    repo: "https://github.com/Vasanth26102004/DailyDo.git",
+    preview: "https://daily-do-app.vercel.app",
+    technologies: "HTML, CSS, JAVASCRIPT, REACT, NODE, EXPRESS",
   },
   {
     id: "1",
