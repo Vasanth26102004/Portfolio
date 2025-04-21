363b972d25481e0bdb474e6897c12d2cebe5d57c
Vasanth <157830407+Vasanth26102004@users.noreply.github.com>
2025-04-22 01:38:11 +0530

    Update project.js

--- src/project.js
+++ src/project.js
@@ -4,69 +4,69 @@ const project = [
     name: "Whack-A-Mole",
     description:
       "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
-    image: "https://www.marketjs.com/item/whack-a-mole/whack-a-mole.jpg",
+    image: "../assets/shots/whack-a-mole.png",
     repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
     preview: "https://vasanth26102004.github.io/Whack-A-Mole/",
     technologies: "HTML, CSS, JAVASCRIPT",
   },
   {
     id: "2",
-    name: "Calculater",
+    name: "Calculator",
     description:
-      "A calculator app is a software tool designed to perform basic arithmetic operations (addition, subtraction, multiplication, division)",
-    image: "https://www.marketjs.com/item/whack-a-mole/whack-a-mole.jpg",
-    repo: "https://github.com/Vasanth26102004/Calculater.git",
+      "This is a Simple Calculater using Javascript with Basic Arithmatic Operations such as Addition Subraction Multiplication & Division.",
+    image: "../assets/shots/calculater.png",
+    repo: "https://github.com/Vasanth26102004/Calculater",
     preview: "https://vasanth26102004.github.io/Calculater/",
     technologies: "HTML, CSS, JAVASCRIPT",
   },
   {
     id: "3",
-    name: "Rock-Paper-Scissor",
+    name: "Shopper",
     description:
-      "Rock Paper Scissors is a simple and well-known hand game often used as a decision-making tool. It is typically played between two people, each of whom simultaneously forms one of three possible shapes with an outstretched hand. ",
-    image: "https://www.marketjs.com/item/whack-a-mole/whack-a-mole.jpg",
-    repo: "https://github.com/Vasanth26102004/RockPaperScissor.git",
+      "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
+    image: "../assets/shots/shopper.png",
+    repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
+    preview: "",
+    technologies: "HTML, CSS, JAVASCRIPT",
+  },
+  {
+    id: "4",
+    name: "Daily-Do",
+    description:
+      "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
+    image: "../assets/shots/daily-do.png",
+    repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
+    preview: "https://daily-do-app.vercel.app/",
+    technologies: "HTML, CSS, JAVASCRIPT",
+  },
+  {
+    id: "5",
+    name: "Photography",
+    description:
+      "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
+    image: "../assets/shots/daily-do.png",
+    repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
+    preview: "https://duraiphotography.netlify.app/",
+    technologies: "HTML, CSS, JAVASCRIPT",
+  },
+  {
+    id: "6",
+    name: "Floppy Bird",
+    description:
+      "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
+    image: "../assets/shots/floppy-bird.png",
+    repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
+    preview: "https://vasanth26102004.github.io/FloppyBird/",
+    technologies: "HTML, CSS, JAVASCRIPT",
+  },
+  {
+    id: "7",
+    name: "Rock-Paper-Sciccor",
+    description:
+      "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
+    image: "../assets/shots/rock-paper-scissor.png",
+    repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
     preview: "https://vasanth26102004.github.io/RockPaperScissor/",
-    technologies: "HTML, CSS, JAVASCRIPT",
-  },
-  {
-    id: "1",
-    name: "Daily Do",
-    description:
-      "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
-    image: "https://www.marketjs.com/item/whack-a-mole/whack-a-mole.jpg",
-    repo: "https://github.com/Vasanth26102004/DailyDo.git",
-    preview: "https://daily-do-app.vercel.app",
-    technologies: "HTML, CSS, JAVASCRIPT, REACT, NODE, EXPRESS",
-  },
-  {
-    id: "1",
-    name: "Whack-A-Mole",
-    description:
-      "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
-    image: "https://www.marketjs.com/item/whack-a-mole/whack-a-mole.jpg",
-    repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
-    preview: "https://vasanth26102004.github.io/Whack-A-Mole/",
-    technologies: "HTML, CSS, JAVASCRIPT",
-  },
-  {
-    id: "1",
-    name: "Whack-A-Mole",
-    description:
-      "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
-    image: "https://www.marketjs.com/item/whack-a-mole/whack-a-mole.jpg",
-    repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
-    preview: "https://vasanth26102004.github.io/Whack-A-Mole/",
-    technologies: "HTML, CSS, JAVASCRIPT",
-  },
-  {
-    id: "1",
-    name: "Whack-A-Mole",
-    description:
-      "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
-    image: "https://www.marketjs.com/item/whack-a-mole/whack-a-mole.jpg",
-    repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
-    preview: "https://vasanth26102004.github.io/Whack-A-Mole/",
     technologies: "HTML, CSS, JAVASCRIPT",
   },
 ];
