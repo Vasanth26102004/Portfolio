1548814a32bdc222e9287bd8244aa63ff74fc683
Vasanth26102004 <vasanth.p2610@gmil.com>
2025-05-13 23:39:48 +0530

    haha

--- src/project.js
+++ src/project.js
@@ -4,7 +4,7 @@ const project = [
     name: "Whack-A-Mole",
     description:
       "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
-    image: "../assets/shots/whack-a-mole.png",
+    image: "./src/assets/shots/whack-a-mole.png",
     repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
     preview: "https://vasanth26102004.github.io/Whack-A-Mole/",
     technologies: "HTML, CSS, JAVASCRIPT",
@@ -14,7 +14,7 @@ const project = [
     name: "Calculator",
     description:
       "This is a Simple Calculater using Javascript with Basic Arithmatic Operations such as Addition Subraction Multiplication & Division.",
-    image: "../assets/shots/calculater.png",
+    image: "./src/assets/shots/calculater.png",
     repo: "https://github.com/Vasanth26102004/Calculater",
     preview: "https://vasanth26102004.github.io/Calculater/",
     technologies: "HTML, CSS, JAVASCRIPT",
@@ -24,7 +24,7 @@ const project = [
     name: "Shopper",
     description:
       "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
-    image: "../assets/shots/shopper.png",
+    image: "./src/assets/shots/shopper.png",
     repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
     preview: "",
     technologies: "HTML, CSS, JAVASCRIPT",
@@ -34,7 +34,7 @@ const project = [
     name: "Daily-Do",
     description:
       "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
-    image: "../assets/shots/daily-do.png",
+    image: "./src/assets/shots/daily-do.png",
     repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
     preview: "https://daily-do-app.vercel.app/",
     technologies: "HTML, CSS, JAVASCRIPT",
@@ -44,7 +44,7 @@ const project = [
     name: "Photography",
     description:
       "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
-    image: "../assets/shots/daily-do.png",
+    image: "./src/assets/shots/photography.png",
     repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
     preview: "https://duraiphotography.netlify.app/",
     technologies: "HTML, CSS, JAVASCRIPT",
@@ -54,7 +54,7 @@ const project = [
     name: "Floppy Bird",
     description:
       "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
-    image: "../assets/shots/floppy-bird.png",
+    image: "./src/assets/shots/floppy-bird.png",
     repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
     preview: "https://vasanth26102004.github.io/FloppyBird/",
     technologies: "HTML, CSS, JAVASCRIPT",
@@ -64,7 +64,7 @@ const project = [
     name: "Rock-Paper-Sciccor",
     description:
       "This is a Simple Whack-A-Mole Game that was created using Javascript. It is a simple game where you have to hit the mole that pops up from the hole and Avoid the Man Eater Plant. ",
-    image: "../assets/shots/rock-paper-scissor.png",
+    image: "./src/assets/shots/rock-paper-scissor.png",
     repo: "https://github.com/Vasanth26102004/Whack-A-Mole.git",
     preview: "https://vasanth26102004.github.io/RockPaperScissor/",
     technologies: "HTML, CSS, JAVASCRIPT",
